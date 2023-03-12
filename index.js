62ae36dc0435daa203be59cf2e1cc800ee166f22
Olaf Mihari <olafnjenga@gmail.com>
2023-03-12 06:48:34 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+const multiply = (num1 * num2);
+
+const random = Math.ceil(Math.random());
+
+let num3 = 36;
+let num4 = 8;
+const mod = (num3%num4);
+
+const max = Math.max(16,17,18,19,20);
